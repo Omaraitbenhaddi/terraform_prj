84660923111c77368defe00ddf684674ded7b58b
OMAR AIT BENHADDI <88466716+Omaraitbenhaddi@users.noreply.github.com>
2024-04-22 16:32:37 +0000

    ajouter datasource et les template de provisionement

--- main.tf
+++ main.tf
@@ -67,3 +67,38 @@ resource "aws_security_group" "mtc_sg" {
 
 }
 
+resource "aws_key_pair" "mtc_auth" {
+  key_name   = "mtckey"
+  public_key = file("~/.ssh/mtckey.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.server_ami.id
+  key_name               = aws_key_pair.mtc_auth.id
+  vpc_security_group_ids = [aws_security_group.mtc_sg.id]
+  subnet_id              = aws_subnet.mtc_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  tags = {
+    Name = "dev-node"
+  }
+
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+    echo "${templatefile("${var.host_os}-ssh-config.tpl", {
+    hostname     = self.public_ip,
+    user         = "ubuntu",
+    identityFile = "~/.ssh/mtckey"
+})}" >> ~/.ssh/config
+  EOT
+}
+
+
+}
+
